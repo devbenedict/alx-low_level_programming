2adc21f1bd4704cc375b2ecae29290cd5fb0fdc3
devbenedict <benniedrille@gmail.com>
2022-10-14 19:31:57 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry
- * return: Always 
+ * return: Always
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+
